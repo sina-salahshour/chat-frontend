f6345fe0203d45647aa26104267d52f591c642da
Sina Salahshour <sina.salahshour.32@gmail.com>
2023-09-02 18:18:54 +0330

    feat: extend tailwind config to match theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,22 @@ const config: Config = {
         black: "#000000",
         white: "#FFFFFF",
         primary:"#1F222B",
+        "primary-hover": "#2D313E",
         secondary: "#909090",
+      },
+      boxShadow: {
+        card :"0px 10px 20px 0px rgba(0, 0, 0, 0.25)",
+        "accent-glow":"0px 0px 20px 0px rgba(11, 248, 148, 0.50);"
+      },
+      data: {
+        primary: 'variant="primary"',
+        secondary: 'variant="secondary"',
+        normal: 'size="normal"',
+        compact: 'size="compact"',
+      },
+      spacing: {
+        "base-width": "320px",
+        "base-height": "480px"
       }
     },
   },
