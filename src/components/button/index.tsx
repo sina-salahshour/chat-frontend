bbd1e0b7849208edd15a3b54e2983fcb3ab6bd3c
Sina Salahshour <sina.salahshour.32@gmail.com>
2023-09-02 18:19:18 +0330

    feat: add button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,23 @@
+import { cn } from "utils/cn";
+import React from "react";
+import type { ComponentProps } from "react";
+
+export interface ButtonProps extends ComponentProps<"button"> {
+    variant?: "primary" | "secondary";
+    size?: "normal" | "compact";
+}
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ variant = "primary", size = "normal", className, ...props }, ref) =>
+    (<button data-variant={variant} data-size={size} ref={ref} {...props}
+        className={cn(
+            "flex flex-col items-center justify-center font-semibold transition-all disabled:opacity-50",
+            "rounded data-primary:bg-accent-default data-primary:text-background",
+            "data-primary:hover:shadow-accent-glow data-primary:active:shadow-none data-primary:disabled:shadow-none",
+            "data-secondary:bg-primary data-secondary:text-secondary data-secondary:hover:bg-primary-hover data-secondary:active:text-white",
+            "data-secondary:disabled:bg-primary data-secondary:disabled:text-secondary",
+            "data-normal:h-10 data-normal:px-4 data-normal:text-sm data-normal:leading-[18px]",
+            "data-compact:h-6 data-compact:text-[12px] data-compact:leading-4",
+            className
+        )}
+    />));
+Button.displayName = "Button";
