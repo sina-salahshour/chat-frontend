cb76d27e15e63878290a66c6c6d735c4e9be16a2
Sina Salahshour <sina.salahshour.32@gmail.com>
2023-09-04 16:22:22 +0330

    fix: document formatting

--- src/components/icon-button/index.tsx
+++ src/components/icon-button/index.tsx
@@ -12,7 +12,7 @@ export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(({ icon
     <button
         {...props}
         ref={ref}
-        className={cn("group flex items-center text-secondary hover:text-white active:text-accent-active disabled:opacity-50 data-[has-text=true]:gap-2", className)}
+        className={cn("group flex items-center text-secondary data-[has-text=true]:gap-2 hover:text-white active:text-accent-active disabled:opacity-50", className)}
         data-has-text={!!children}
     >
         <Icon className="[&_path]:fill-secondary group-hover:[&_path]:fill-white group-active:[&_path]:fill-accent-active" /> <div>{children}</div>
